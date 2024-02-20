0e0831ec9a095d19b244ae10aafed968fa951a3b
Andrew Cook <acook999@gmail.com>
2024-02-20 11:05:10 -0600

    cleanup before branch

--- website/models.py
+++ website/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Modal(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=200, db_column='title', blank=True, null=True)  # Field name made lowercase.
+    content = models.CharField(max_length=2048, db_column='content', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = True
+        db_table = 'modal'
+
 # class MyRouter(object):
 #     def db_for_read(self, model, **hints):
 #         """
