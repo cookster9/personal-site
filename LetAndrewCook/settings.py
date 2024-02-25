dd1f8e67fbf083488815fb34e6662e7eda7dd14c
Andrew Cook <acook999@gmail.com>
2024-02-25 15:22:30 -0600

    static directory

--- LetAndrewCook/settings.py
+++ LetAndrewCook/settings.py
@@ -15,6 +15,7 @@ from decouple import config
 import pymysql
 pymysql.version_info = (1, 4, 3, "final", 0)
 pymysql.install_as_MySQLdb()
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,6 +138,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = config('STATIC_ROOT', default=os.path.join(BASE_DIR, 'static'))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
