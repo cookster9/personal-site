6e81ad5a9317d8ac4226a22542bf908d8237f016
cookster9 <acook999@gmail.com>
2024-12-22 20:32:23 -0700

    Apps cards look nice

--- website/urls.py
+++ website/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.welcome, name='welcome'),
-    path('contact/', views.contact_page, name='contact'),
     path('resume/', views.resume_page, name='resume'),
+    path('apps/', views.apps_page, name='apps'),
+    path('contact/', views.contact_page, name='contact'),
     path("success/", views.success, name="success"),
 ]
