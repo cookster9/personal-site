36bef32bd0dcecebacfad82ab6813abf53d133e1
cookster9 <acook999@gmail.com>
2025-01-06 20:57:51 -0600

    update the app preview, remembered had to update tailwind config for new base template location

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./website/**/*.{html,js}'],
+  content: ['./website/**/*.{html,js}','./templates/*.html'],
   theme: {
     extend: {},
   },
